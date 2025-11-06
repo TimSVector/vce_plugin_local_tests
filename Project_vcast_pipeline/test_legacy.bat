af96a3a970891bff9ee06ccd21f20eea0ee03caf
Tim Schneider <tim.schneider@vector.com>
2025-11-06 13:54:17 -0500

    Update test_legacy.bat to log new test runs

--- Project_vcast_pipeline/test_legacy.bat
+++ Project_vcast_pipeline/test_legacy.bat
@@ -1,9 +1,13 @@
 del ..\*all_test.log /y
 
-call suite_cbt_legacy.test_cbt_2018.bat     > ..\2018_all_test.log 2>&1
-call suite_cbt_legacy.test_cbt_2019.bat     > ..\2019_all_test.log 2>&1
-call suite_cbt_legacy.test_cbt_2020.bat     > ..\2020_all_test.log 2>&1
-call suite_cbt_legacy.test_cbt_2023.bat     > ..\2023_all_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2018.bat     > ..\2018sp5_legacy_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2019.bat     > ..\2019_legacy_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2019sp6.bat  > ..\2019sp6_legacy_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2020sp1.bat  > ..\2020sp1_legacy_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2020.bat     > ..\2020sp7_legacy_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2023.bat     > ..\2023sp5_legacy_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2024.bat     > ..\2024sp6_legacy_test.log 2>&1
+call suite_cbt_legacy.test_cbt_2025.bat     > ..\2025sp4_legacy_test.log 2>&1
 
 call npp ..\*all_test.log
 
