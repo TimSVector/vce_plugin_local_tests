ce7f169aea94811d3645e747ef1303179d92aa46
TimSVector <tim.schneider@vector.com>
2026-02-08 16:36:02 -0500

    update for >0% mcdc

--- Project_vcast_pipeline/working_dir/unit.adb
+++ Project_vcast_pipeline/working_dir/unit.adb
@@ -1,13 +1,16 @@
 package body unit is
 
-   function isPositive( X : Integer ) return Boolean is
+   function IsPositive (X : Integer) return Boolean is
    begin
       if X < 0 then
          return False;
-      else
+      elsif X > 0 and X < 100 then
          return True;
+      elsif X > 100 and X < 1000 then
+         return True;
+      else
+         return False;
       end if;
-   end isPositive;
+   end IsPositive;
 
 end unit;
-
