5b4dfa430fd5b898761240a46cd3357d36621f13
TimSVector <tim.schneider@vector.com>
2026-03-11 11:20:46 -0400

    updating project path for system_test.py

--- Project_vcast_pipeline/minimum_cleanup_and_build.bat
+++ Project_vcast_pipeline/minimum_cleanup_and_build.bat
@@ -18,13 +18,13 @@ call ENV_COVER.bat
 set /p FIRSTTOKEN=<%VECTORCAST_DIR%\DATA\tool_version.txt
 
 if "%FIRSTTOKEN:~0,2%"=="18" (
-    copy /Y system_tests.py Project\python\system_tests.py
+    copy /Y system_tests.py ..\Project\python\system_tests.py
 ) else if "%FIRSTTOKEN:~0,2%"=="19" (
-    copy /Y system_tests_2019.py Project\python\ENV_COVER_system_tests.py
+    copy /Y system_tests_2019.py ..\Project\python\ENV_COVER_system_tests.py
 ) else if "%FIRSTTOKEN:~0,2%"=="20" (
-    copy /Y ENV_COVER_system_tests_2020sp1.py Project\python\ENV_COVER_system_tests.py
+    copy /Y ENV_COVER_system_tests_2020sp1.py ..\Project\python\ENV_COVER_system_tests.py
 ) else (
-    copy /Y ENV_COVER_system_tests.py Project\python\ENV_COVER_system_tests.py
+    copy /Y ENV_COVER_system_tests.py ..\Project\python\ENV_COVER_system_tests.py
 )
 cd ..
 
