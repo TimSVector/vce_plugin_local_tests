047bb94a199dac3686d01dccfbc6aec212371853
TimSVector <tim.schneider@vector.com>
2026-03-11 11:37:25 -0400

    updates still for moving regressions

--- Project_vcast_pipeline/minimum_cleanup_and_build.bat
+++ Project_vcast_pipeline/minimum_cleanup_and_build.bat
@@ -19,13 +19,22 @@ set /p FIRSTTOKEN=<%VECTORCAST_DIR%\DATA\tool_version.txt
 
 if "%FIRSTTOKEN:~0,2%"=="18" (
     copy /Y system_tests.py ..\Project\python\system_tests.py
-) else if "%FIRSTTOKEN:~0,2%"=="19" (
+    goto SKIP_OTHER_COPIES
+) 
+
+if "%FIRSTTOKEN:~0,2%"=="19" (
     copy /Y system_tests_2019.py ..\Project\python\ENV_COVER_system_tests.py
-) else if "%FIRSTTOKEN:~0,2%"=="20" (
+    goto SKIP_OTHER_COPIES
+) 
+
+if "%FIRSTTOKEN:~0,2%"=="20" (
     copy /Y ENV_COVER_system_tests_2020sp1.py ..\Project\python\ENV_COVER_system_tests.py
 ) else (
     copy /Y ENV_COVER_system_tests.py ..\Project\python\ENV_COVER_system_tests.py
 )
+
+:SKIP_OTHER_COPIES
+
 cd ..
 
 gcc -c unit.adb
